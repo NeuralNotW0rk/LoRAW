2a3b1212d9de479e60cbdd84e0b6959306b4109e
Bikecicle <griffinpage9@gmail.com>
2023-07-20 15:36:16 -0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='loraw',
+    version='1.0.0',
+    url='',
+    author='Griffin Page',
+    packages=find_packages(),    
+    install_requires=[
+        'einops',
+        'pandas',
+        'prefigure', 
+        'pytorch_lightning',
+        'scipy',
+        'torch',
+        'torchaudio',
+        'tqdm',
+        'wandb',
+    ],
+)
